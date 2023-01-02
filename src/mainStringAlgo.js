6a91ec139a5771bd33eef4df5a0bb8b388598566
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2023-01-02 13:17:28 -0500

    lift state to parent component
    -so that ahoautomaton can see mainstring that was inputted

--- src/mainStringAlgo.js
+++ src/mainStringAlgo.js
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 
-const MainStringInput = () => {
+const MainStringInput = ({setMainstring}) => {
+
     const [str, setStr] = useState("");
 
     function getData(event) {
         setStr(event.target.value);
     }
 
-    function ahoCorasick() {
-        console.log("d")
-    }
-    
     function handleClick(e) {
         e.preventDefault();
+        setMainstring(str);
     }
 
 
