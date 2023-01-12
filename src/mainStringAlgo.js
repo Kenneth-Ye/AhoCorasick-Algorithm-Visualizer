788be5652520ac0ee681dee9549b40683efad950
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2023-01-12 18:20:38 -0500

    add error checking

--- src/mainStringAlgo.js
+++ src/mainStringAlgo.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import './App.css';
+import EmptyString from './emptyStringError';
 
 const MainStringInput = ({setMainstring}) => {
-
-    const [str, setStr] = useState("");
+    const [empty, setEmpty] = useState(false);
+    var str = ""
 
     function getData(event) {
-        setStr(event.target.value);
+        str = event.target.value;
     }
 
     function handleClick(e) {
         e.preventDefault();
-        setMainstring(str);
+        if (str === "") {
+            setEmpty(true);
+            return; //if string is empty do not add to array
+          }
+          else {
+            setEmpty(false);
+            setMainstring(str);
+        }
     }
 
 
@@ -20,7 +28,10 @@ const MainStringInput = ({setMainstring}) => {
             <form>
                 <label>Enter Main String </label>
                 <input type="text" onChange={getData}/> 
-                <button type="inputButton" onClick={handleClick}>Visualize!</button>
+                <button className="inputButton" onClick={handleClick}>Set Mainstring</button>
+                <div className="mainstringEmpty">
+                    <EmptyString showError = {empty}></EmptyString>
+                </div>
             </form>
         </div> 
     );
