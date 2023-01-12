d63d0c12320122fd51927813f18f3248b1c5ec87
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2023-01-12 18:23:05 -0500

    import useResizeObserver hook
    imported from https://github.com/muratkemaldar

---
+++ src/useResizeObserver.js
@@ -0,0 +1,24 @@
+//import resize observer component from github
+
+import ResizeObserver from "resize-observer-polyfill";
+import { useState, useEffect } from 'react';
+
+//import useResizeObserver from Muratorium on Github
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect);
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
+
+export default useResizeObserver;
